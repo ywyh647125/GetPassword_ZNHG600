15ac3d4fc80e1a7fe3e0551865872d275db82560
kenichi2022q <143829423+kenichi2022q@users.noreply.github.com>
2023-09-02 00:39:58 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -298,7 +298,7 @@ function getPassword()
       console.log(i+"/99999999");
     }
     var result = hex_md5(name);
-    if (result=="")
+    if (result=="ABCDEFGHIJKL")
     {
       console.log(name);
       break;
